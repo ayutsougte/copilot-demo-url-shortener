9708d796dc74972d8cc3adeae8fe2f163f516147
Alex <a.yutsou@godeltech.com>
2026-03-02 00:31:46 +0100

    feat: SQLite database setup and schema design (#1)

--- src/ShortLinkApp.Api/Program.cs
+++ src/ShortLinkApp.Api/Program.cs
@@ -1,10 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using ShortLinkApp.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
+// Ensure the database file is created on first run.
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
